9da3c4a551a8c71110703c9dac3adc9a8fcacfca
arun <arunorrun@gmail.com>
2024-03-13 08:48:05 -0400

    project6 commit

---
+++ src/main/java/sfr/MyWebApplication.java
@@ -0,0 +1,16 @@
+package sfr;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPackage("sfr.web");
+		registerModule(DataModel.class);
+	}
+	
+}
